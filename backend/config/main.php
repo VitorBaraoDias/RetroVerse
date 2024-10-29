da9c3ac384d55ab8600adec4ea585f08096b3ab7
vitor <vitorbaraotbd@gmail.com>
2024-10-29 11:53:38 +0000

    Modulo da api

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,7 +11,11 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
-    'modules' => [],
+    'modules' => [
+        'api' => [
+            'class' => 'backend\modules\api\ModuleAPI',
+        ]
+    ],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
@@ -37,14 +41,17 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                //mudar nome do controller
+
+                ['class' => 'yii\rest\UrlRule','controller' => 'api/user'],
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
