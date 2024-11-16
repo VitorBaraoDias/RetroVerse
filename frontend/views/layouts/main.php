d3a54366c1ec68579b063e33160c141c95166724
tomas-ricardo <tomasricardo.main@gmail.com>
2024-11-16 15:27:04 +0000

    Frontoffice com NavBar conluida

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -11,55 +11,40 @@ use yii\bootstrap5\Nav;
 use yii\bootstrap5\NavBar;
 
 AppAsset::register($this);
+
+$action = Yii::$app->controller->id;
+
 ?>
 <?php $this->beginPage() ?>
+
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>" class="h-100">
 <head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <?php $this->registerCsrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
+    <meta charset="UTF-8">
+    <meta name="description" content="Retroverse second hand clothes">
+    <meta name="keywords" content="Vintage clothing, retro, y2k, Retroverse, retroverse clothing">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RetroVerse | Homepage</title>
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>/css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>/css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>/css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>/css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>/css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="<?= Yii::getAlias('@web') ?>/css/style.css" type="text/css">
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
 </head>
+
 <body class="d-flex flex-column h-100">
+
 <?php $this->beginBody() ?>
-
-<header>
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar navbar-expand-md navbar-dark bg-dark fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
-    ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-    }
-
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
-        'items' => $menuItems,
-    ]);
-    if (Yii::$app->user->isGuest) {
-        echo Html::tag('div',Html::a('Login',['/site/login'],['class' => ['btn btn-link login text-decoration-none']]),['class' => ['d-flex']]);
-    } else {
-        echo Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout text-decoration-none']
-            )
-            . Html::endForm();
-    }
-    NavBar::end();
-    ?>
-</header>
+<?= $this->render('navbar') ?>
 
 <main role="main" class="flex-shrink-0">
     <div class="container">
@@ -71,14 +56,84 @@ AppAsset::register($this);
     </div>
 </main>
 
-<footer class="footer mt-auto py-3 text-muted">
+<!-- Footer Section Begin -->
+<footer class="footer">
     <div class="container">
-        <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-        <p class="float-end"><?= Yii::powered() ?></p>
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="footer__about">
+                    <div class="footer__logo">
+                        <a href="#"><img src="<?= Yii::getAlias('@web') ?>/img/footer-logo.png" alt=""></a>
+                    </div>
+                    <p>The customer is at the heart of our unique business model, which includes design.</p>
+                    <a href="#"><img src="<?= Yii::getAlias('@web') ?>/img/payment.png" alt=""></a>
+                </div>
+            </div>
+            <div class="col-lg-2 offset-lg-1 col-md-3 col-sm-6">
+                <div class="footer__widget">
+                    <h6>Shopping</h6>
+                    <ul>
+                        <li><a href="#">Clothing Store</a></li>
+                        <li><a href="#">Trending Shoes</a></li>
+                        <li><a href="#">Accessories</a></li>
+                        <li><a href="#">Sale</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-6">
+                <div class="footer__widget">
+                    <h6>Shopping</h6>
+                    <ul>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Payment Methods</a></li>
+                        <li><a href="#">Delivary</a></li>
+                        <li><a href="#">Return & Exchanges</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 offset-lg-1 col-md-6 col-sm-6">
+                <div class="footer__widget">
+                    <h6>NewLetter</h6>
+                    <div class="footer__newslatter">
+                        <p>Be the first to know about new arrivals, look books, sales & promos!</p>
+                        <form action="#">
+                            <input type="text" placeholder="Your email">
+                            <button type="submit"><span class="icon_mail_alt"></span></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="footer__copyright__text">
+                    <p>Copyright ©
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script>2020
+                        All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </footer>
+<!-- Footer Section End -->
 
 <?php $this->endBody() ?>
+
 </body>
+
+<!-- Js Plugins -->
+<script src="<?= Yii::getAlias('@web') ?>/js/jquery-3.3.1.min.js"></script>
+<script src="<?= Yii::getAlias('@web') ?>/js/bootstrap.min.js"></script>
+<script src="<?= Yii::getAlias('@web') ?>/js/jquery.nice-select.min.js"></script>
+<script src="<?= Yii::getAlias('@web') ?>/js/jquery.nicescroll.min.js"></script>
+<script src="<?= Yii::getAlias('@web') ?>/js/jquery.magnific-popup.min.js"></script>
+<script src="<?= Yii::getAlias('@web') ?>/js/jquery.countdown.min.js"></script>
+<script src="<?= Yii::getAlias('@web') ?>/js/jquery.slicknav.js"></script>
+<script src="<?= Yii::getAlias('@web') ?>/js/mixitup.min.js"></script>
+<script src="<?= Yii::getAlias('@web') ?>/js/owl.carousel.min.js"></script>
+<script src="<?= Yii::getAlias('@web') ?>/js/main.js"></script>
+
 </html>
-<?php $this->endPage();
