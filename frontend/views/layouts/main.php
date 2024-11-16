6b995d9375133ac0683cfa24fe5991c9efa15bd2
tomas-ricardo <tomasricardo.main@gmail.com>
2024-11-16 17:16:55 +0000

    Footer concluido

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -49,7 +49,7 @@ $action = Yii::$app->controller->id;
 
 <main role="main" class="flex-shrink-0">
 
-    <div class="container">
+    <div class="container-fluid">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
@@ -62,62 +62,48 @@ $action = Yii::$app->controller->id;
 <footer class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-lg-3 col-md-6 col-sm-6">
+            <div class="col-lg-3 col-md-12 col-sm-12 pr-5">
                 <div class="footer__about">
                     <div class="footer__logo">
-                        <a href="#"><img src="<?= Yii::getAlias('@web') ?>/img/footer-logo.png" alt=""></a>
+                        <a href="#"><img src="<?= Yii::getAlias('@web') ?>/img/retroverse-logo.svg" alt=""></a>
                     </div>
-                    <p>The customer is at the heart of our unique business model, which includes design.</p>
-                    <a href="#"><img src="<?= Yii::getAlias('@web') ?>/img/payment.png" alt=""></a>
+                    <a href="#"><img src="<?= Yii::getAlias('@web') ?>/img/footer/payments.png" alt=""></a>
                 </div>
             </div>
-            <div class="col-lg-2 offset-lg-1 col-md-3 col-sm-6">
+            <div class="col-lg-3 col-md-4 col-sm-12">
                 <div class="footer__widget">
-                    <h6>Shopping</h6>
+                    <h6>NAVIGATION</h6>
                     <ul>
-                        <li><a href="#">Clothing Store</a></li>
-                        <li><a href="#">Trending Shoes</a></li>
-                        <li><a href="#">Accessories</a></li>
-                        <li><a href="#">Sale</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Collection</a></li>
+                        <li><a href="#">Plans</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">FAQ</a></li>
                     </ul>
                 </div>
             </div>
-            <div class="col-lg-2 col-md-3 col-sm-6">
+            <div class="col-lg-3 col-md-4 col-sm-12">
                 <div class="footer__widget">
-                    <h6>Shopping</h6>
+                    <h6>MARKETPLACE</h6>
                     <ul>
-                        <li><a href="#">Contact Us</a></li>
-                        <li><a href="#">Payment Methods</a></li>
-                        <li><a href="#">Delivary</a></li>
-                        <li><a href="#">Return & Exchanges</a></li>
+                        <li><a href="#">Collection</a></li>
+                        <li><a href="#">Sell an Item</a></li>
+
                     </ul>
                 </div>
             </div>
-            <div class="col-lg-3 offset-lg-1 col-md-6 col-sm-6">
+            <div class="col-lg-3 col-md-4 col-sm-12">
                 <div class="footer__widget">
-                    <h6>NewLetter</h6>
-                    <div class="footer__newslatter">
-                        <p>Be the first to know about new arrivals, look books, sales & promos!</p>
-                        <form action="#">
-                            <input type="text" placeholder="Your email">
-                            <button type="submit"><span class="icon_mail_alt"></span></button>
-                        </form>
-                    </div>
+                    <h6>LEGAL</h6>
+                    <ul>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Terms of Service</a></li>
+
+                    </ul>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-lg-12 text-center">
-                <div class="footer__copyright__text">
-                    <p>Copyright ©
-                        <script>
-                            document.write(new Date().getFullYear());
-                        </script>2020
-                        All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                    </p>
-                </div>
-            </div>
-        </div>
+
     </div>
 </footer>
 <!-- Footer Section End -->
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -8,17 +8,9 @@
                     <div class="row">
                         <div class="col-xl-5 col-lg-7 col-md-8">
                             <div class="hero__text">
-                                <h6>Summer Collection</h6>
-                                <h2>Fall - Winter Collections 2030</h2>
-                                <p>A specialist label creating luxury essentials. Ethically crafted with an unwavering
-                                commitment to exceptional quality.</p>
-                                <a href="#" class="primary-btn">Shop now <span class="arrow_right"></span></a>
-                                <div class="hero__social">
-                                    <a href="#"><i class="fa fa-facebook"></i></a>
-                                    <a href="#"><i class="fa fa-twitter"></i></a>
-                                    <a href="#"><i class="fa fa-pinterest"></i></a>
-                                    <a href="#"><i class="fa fa-instagram"></i></a>
-                                </div>
+                                <h2>WINTER DROP</h2>
+                                <p>Our latest winter drop is already available</p>
+                                <a href="#" class="primary-btn btn_banner">SHOP NOW<span class="arrow_right"></span></a>
                             </div>
                         </div>
                     </div>
@@ -32,12 +24,6 @@
                                 <h2>ADIDAS VINTAGE WINDBREAKERS</h2>
                                 <p>Our new Adidas Vintage Windbreakers are now available in our store!</p>
                                 <a href="#" class="primary-btn btn_banner">SHOW NOW <span class="arrow_right"></span></a>
-                                <div class="hero__social">
-                                    <a href="#"><i class="fa fa-facebook"></i></a>
-                                    <a href="#"><i class="fa fa-twitter"></i></a>
-                                    <a href="#"><i class="fa fa-pinterest"></i></a>
-                                    <a href="#"><i class="fa fa-instagram"></i></a>
-                                </div>
                             </div>
                         </div>
                     </div>
@@ -47,47 +33,6 @@
     </section>
     <!-- Hero Section End -->
 
-    <!-- Banner Section Begin -->
-    <section class="banner spad">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-7 offset-lg-4">
-                    <div class="banner__item">
-                        <div class="banner__item__pic">
-                            <img src="../../web/img/banner/banner-1.jpg" alt="">
-                        </div>
-                        <div class="banner__item__text">
-                            <h2>Clothing Collections 2030</h2>
-                            <a href="#">Shop now</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-5">
-                    <div class="banner__item banner__item--middle">
-                        <div class="banner__item__pic">
-                            <img src="../../web/img/banner/banner-2.jpg" alt="">
-                        </div>
-                        <div class="banner__item__text">
-                            <h2>Accessories</h2>
-                            <a href="#">Shop now</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-7">
-                    <div class="banner__item banner__item--last">
-                        <div class="banner__item__pic">
-                            <img src="../../web/img/banner/banner-3.jpg" alt="">
-                        </div>
-                        <div class="banner__item__text">
-                            <h2>Shoes Spring 2030</h2>
-                            <a href="#">Shop now</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Banner Section End -->
 
     <!-- Product Section Begin -->
     <section class="product spad">
