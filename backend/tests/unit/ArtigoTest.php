effb2c37213770f03122885b44a7457af3f4235c
tomas-ricardo <tomasricardo.main@gmail.com>
2025-01-08 15:50:31 +0000

    TEST not working

--- backend/tests/unit/ArtigoTest.php
+++ backend/tests/unit/ArtigoTest.php
@@ -1,6 +1,6 @@
 <?php
 namespace backend\tests\Unit;
-
+use Yii;
 use backend\tests\Unit;
 use common\models\Artigo;
 
@@ -10,7 +10,7 @@ class ArtigoTest extends \Codeception\Test\Unit
     public function testRequiredFields()
     {
         $artigo = new Artigo();
-        $artigo->nome = "ola";
+        $artigo->descricao = "ola";
 
         $this->assertFalse($artigo->validate(['nome']));
     }
--- common/config/codeception-local.php
+++ common/config/codeception-local.php
@@ -11,6 +11,13 @@ return yii\helpers\ArrayHelper::merge(
                 // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
                 'cookieValidationKey' => 'Gr8-9G8ZVIzBHfZ3v90LI1cgUUcopSTy',
             ],
+            'db' => [
+                'class' => \yii\db\Connection::class,
+                'dsn' => 'mysql:host=localhost;dbname=retroverse',
+                'username' => 'root',
+                'password' => 'root',
+                'charset' => 'utf8',
+            ],
         ],
     ]
 );
